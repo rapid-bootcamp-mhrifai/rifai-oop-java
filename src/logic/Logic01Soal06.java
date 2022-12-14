a170b898555633031abbffb24b90537b98520134
mhrifai <mkhrifai99@gmail.com>
2022-12-14 09:00:53 +0700

    logic 01

--- src/logic/Logic01Soal06.java
+++ src/logic/Logic01Soal06.java
@@ -5,40 +5,27 @@ public class Logic01Soal06 extends BasicLogic{
         super(n);
     }
 
-    public void isiArray(){
-        int[] deret = new int[this.n];
+    public void isiArray() {
         int a = 0;
 
         for (int i=0; i<= 99; i++){
             int bil=0;
-            for (int j=1;j<=i;j++) {
-                if (i % j == 0) {
-                    bil = bil + 1;
+            for (int j=1;j<=i;j++){
+                if (i%j==0){
+                    bil=bil+1;
                 }
             }
             if (bil==2){
                 a+=1;
-                if(a <= deret.length){
-                    System.out.print(i+"\t");
+                if(a <= this.n){
+                    System.out.print(i+"\t\t");
                 }
             }
-        }System.out.println("\n");
-    }
-        /*for (int i=0; i < this.n; i++) {
-            int angka=0;
-            for(int j=1; j<=i; j++)
-            {
-                if(i%j==0)
-                    angka = angka+1;
-            }
-            if(angka==2)
-                this.array[0][i]= String.valueOf(i);
         }
 
-         */
+    }
 
     public void cetakArray(){
         this.isiArray();
-        this.printSingle();
     }
 }
